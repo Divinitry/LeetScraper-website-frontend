fa0ffbe14355644ae8dba7f004ab8144cb40b838
SMcGoey <SpencerCodeAcc@gmail.com>
2024-09-03 06:35:34 -0400

    set up working api with backend and fetched data example

--- src/components/TDLComponent.jsx
+++ src/components/TDLComponent.jsx
@@ -1,5 +1,3 @@
-
-
 /*     
 make sure you use useHistory for redirecting, use history.push(send them to the page) use this for if they are not authenticated 
 and then make sure you are using useEffect to render things automatically and to render things onto a page when u first load, but REMEMBER 
@@ -7,15 +5,37 @@ useEffect is used to run code automatically and u can control when it runs by if
 whenever the function runs). for deleting things you do not need useEffect, only AUTOMATIC CODE, also remember to send cookies to the backend api to 
 control user session
 */
-
-
+import { useEffect, useState } from "react";
 
 const TDLComponent = () => {
-    return(
+    const [toDoList, setToDoList] = useState([]);
+
+    useEffect(() => {
+        async function getToDoList() {
+            const url = 'http://127.0.0.1:8000/leetscraper/todolist/';
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data);
+                setToDoList(data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+
+        getToDoList(); 
+    }, []);
+
+    return (
         <div className="pt-60">
-            <h1>To Code List</h1>
+            {toDoList.map((item, index) => (
+                <div>
+                    <h1 key={index}>{item.name}</h1>
+                    <p>{item.created_time}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default TDLComponent
+export default TDLComponent;
