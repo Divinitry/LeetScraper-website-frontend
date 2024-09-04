e0a3b6ce7d4af8bc5f236812a0ecaa551f595b42
SMcGoey <SpencerCodeAcc@gmail.com>
2024-09-04 05:54:54 -0400

    some work mostly to reflect new backend route

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,3 +1,14 @@
+
+/*
+use useState to store the search term and send it to the backend and retrieve the data
+and also store the returned data in another useState to send it to the backend and store the data in 
+the database for leetcodequestion, when the button is clicked, if they want to add it to the to-do-list
+*/
+
+
+
+import { useState } from "react"
+
 const SearchComponent = () => {
     return(
         <div className="pt-60">
