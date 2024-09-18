61807356c40c2eb215a1153378cc9db1c58bed37
SMcGoey <SpencerCodeAcc@gmail.com>
2024-09-18 01:26:22 -0400

    finished notes component

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { Button } from "flowbite-react";
+import { Button, Modal } from "flowbite-react";
 import api from "../api";
 
 const Notes = ({ id }) => {
   const [allNotes, setAllNotes] = useState([]);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const getNotes = async () => {
@@ -24,27 +26,87 @@ const Notes = ({ id }) => {
   }, [id]);
 
   const createNote = async () => {
-    try{
-        const response = await api.post(`/todolist/questions/${id}/notes/create/`)
-    }catch(error){
-        console.log(error)
+    try {
+      const response = await api.post(
+        `/leetscraper/todolist/questions/${id}/notes/create/`
+      );
+      const newNote = response.data;
+      setAllNotes((prevNotes) => [...prevNotes, newNote]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteNote = async (nid) => {
+    try {
+      await api.delete(
+        `/leetscraper/todolist/questions/${id}/notes/${nid}/delete/`
+      );
+      setAllNotes((prevNotes) => prevNotes.filter((note) => note.id !== nid));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const openNoteModal = (note) => {
+    setSelectedNote(note);
+    setOpenModal(true);
+  };
+
+  const handleTitleChange = (e) => {
+    setSelectedNote((prevNote) => ({
+      ...prevNote,
+      title: e.target.value,
+    }));
+  };
+
+  const handleBodyChange = (e) => {
+    setSelectedNote((prevNote) => ({
+      ...prevNote,
+      body: e.target.value,
+    }));
+  };
+
+  const saveModalInformation = async () => {
+    try {
+      const response = await api.put(
+        `/leetscraper/todolist/questions/${id}/notes/${selectedNote.id}/update/`,
+        {
+          title: selectedNote.title,
+          body: selectedNote.body,
+        }
+      );
+      const updatedNote = response.data;
+      setAllNotes((prevNotes) =>
+        prevNotes.map((note) =>
+          note.id === updatedNote.id ? updatedNote : note
+        )
+      );
+
+      setOpenModal(false);
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <div>
-      <div className="flex flex-row items-center space-x-3">
-        <p>
-        Notes
-        </p>
-        <button className="bg-transparent hover:bg-white/10 p-1 rounded-full">
+
+      {/* Note title and button to add note here */}
+
+      <div className="flex flex-row items-center justify-center space-x-3 pb-2 pl-1">
+        <p className="text-3xl">Notes</p>
+        <button
+          className="bg-transparent hover:bg-white/10 p-1 rounded-full"
+          onClick={() => createNote()}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-4"
+            className="size-6"
           >
             <path
               strokeLinecap="round"
@@ -54,13 +116,96 @@ const Notes = ({ id }) => {
           </svg>
         </button>
       </div>
-      <div>
+
+      {/* Note itself */}
+
+      <div className="flex flex-row space-x-4 overflow-scroll">
         {allNotes.length === 0 ? (
-          <p>No notes yet</p>
+          <div className="w-80 h-80 bg-white/5 rounded-md flex justify-center items-center"></div>
         ) : (
-          allNotes.map((note, index) => <p key={index}>{note}</p>)
+          allNotes.map((note, index) => (
+            <div
+              key={index}
+              className="min-w-80 w-80 h-80 bg-white/10 rounded-md relative cursor-pointer flex flex-col"
+              onClick={() => openNoteModal(note)}
+            >
+              <div className="flex justify-between p-4">
+                <p className="font-bold">{note.title}</p>
+                <button
+                  className="text-red-500"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteNote(note.id);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="px-4 flex-1 overflow-y-auto">
+                <p className="text-sm h-full">{note.body}</p>
+              </div>
+            </div>
+          ))
         )}
       </div>
+
+      {/* Modal pop up down below */}
+
+      {selectedNote && (
+        <Modal
+          show={openModal}
+          onClose={() => setOpenModal(false)}
+          className="bg-black"
+        >
+          <div className="bg-black/90 border-b-0 flex justify-between items-center p-4">
+            <input
+              className="bg-transparent border-none text-white focus:ring-0"
+              type="text"
+              value={selectedNote.title}
+              onChange={handleTitleChange}
+            />
+
+            <button
+              className="text-white p-3"
+              onClick={() => setOpenModal(false)}
+            >
+              Close
+            </button>
+          </div>
+
+          <Modal.Body className="bg-black/90">
+            <div className="space-y-6">
+              <textarea
+                className="bg-transparent text-white resize-none border-none w-full focus:ring-0 p-1 h-96"
+                value={selectedNote.body}
+                onChange={handleBodyChange}
+              />
+            </div>
+          </Modal.Body>
+
+          <div className="bg-black/90 flex justify-end p-4">
+            <button
+              className="bg-blue-500 text-white p-2 rounded"
+              onClick={saveModalInformation}
+            >
+              Save
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
